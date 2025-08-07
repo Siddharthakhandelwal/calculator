422a7544448acc945c4e2bda8fe16368292c7597
Siddhartha Khandelwal <siddharthakhandelwal9@gmail.com>
2025-08-07 14:30:45 +0530

    added all layers

---
+++ calculator.py
@@ -0,0 +1,24 @@
+def add(x,y):
+    return x + y
+
+
+x= int(input("Enter first number: "))
+y= int(input("Enter second number: "))
+
+print("what do you want to do?")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice=int(input("Enter choice (1/2/3/4): "))
+
+if choice == 1:
+    print("Result:")
+elif choice == 2:
+    print("Result:")
+elif choice == 3:
+    print("Result:")
+elif choice == 4:
+    print("Result:")
+
